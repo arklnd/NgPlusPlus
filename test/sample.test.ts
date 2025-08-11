13bc1a2b39e03ed2a847112b5a6ecde2f9412ec0
Arijit Kundu <Arijit.Kundu@hyland.com>
2025-08-11 14:24:39 +0530

    Squashed commit of the following:
    commit ea0571b2b63e3daa886b7ee7fcf18a4ed00da5ff
    Author: Arijit Kundu <Arijit.Kundu@hyland.com>
    Date:   Sun Aug 10 16:32:54 2025 +0530
    
        add: basic

---
+++ test/sample.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('Sample Test', function () {
+    it('should add numbers correctly', function () {
+        const sum = 1 + 1;
+        expect(sum).to.equal(2);
+    });
+});
