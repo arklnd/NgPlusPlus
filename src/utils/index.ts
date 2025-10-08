291c8912f13f800353069b901f8aa400cbf1f287
Arijit Kundu <Arijit.Kundu@hyland.com>
2025-10-08 12:10:01 +0530

    feat: export dumb-resolver-helper utility in index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,7 @@ export * from '@U/package-json.utils';
 export * from '@U/conflict-resolution.utils';
 export * from '@U/transitive-dependencies.utils';
 export * from '@U/logger.utils';
+export * from '@U/dumb-resolver-helper';
 
 // Export interfaces from the interfaces folder
 export * from '@I/package-registry.interfaces';
